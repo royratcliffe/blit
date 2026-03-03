f2d22d67d607647c7aabfab4a15e82acb35dcfed
Roy Ratcliffe <roy@ratcliffe.me>
2026-03-03 19:50:36 +0000

    Fix Doxygen mainpage and documentation generation settings
    Update the Doxygen configuration to use the current source directory
    for the main page and improve the documentation generation command
    to include the project name.

--- cmake/doxy.cmake
+++ cmake/doxy.cmake
@@ -2,12 +2,13 @@
 # https://www.mcternan.me.uk/mscgen/
 find_package(Doxygen OPTIONAL_COMPONENTS dot mscgen dia)
 if(DOXYGEN_FOUND)
-    set(DOXYGEN_USE_MDFILE_AS_MAINPAGE ${CMAKE_SOURCE_DIR}/README.md)
+    set(DOXYGEN_USE_MDFILE_AS_MAINPAGE ${CMAKE_CURRENT_SOURCE_DIR}/README.md)
     set(DOXYGEN_USE_MATHJAX YES)
     set(DOXYGEN_OPTIMIZE_OUTPUT_FOR_C YES)
     set(DOXYGEN_SOURCE_BROWSER YES)
     set(DOXYGEN_EXCLUDE ${CMAKE_BINARY_DIR})
-    doxygen_add_docs(doxy ${CMAKE_SOURCE_DIR}
-        COMMENT "Generating API documentation with Doxygen"
+    doxygen_add_docs(${PROJECT_NAME}_doxy ${CMAKE_CURRENT_SOURCE_DIR}
+        COMMENT "Generating API documentation with Doxygen for ${PROJECT_NAME}"
     )
+    set_target_properties(${PROJECT_NAME}_doxy PROPERTIES FOLDER "Documentation")
 endif()
